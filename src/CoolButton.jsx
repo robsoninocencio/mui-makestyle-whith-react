ef2315f1e56cba95b5cb8d2ed15715740f41a919
Robson Inocêncio <robson.inocencio@gmail.com>
2021-06-23 02:45:31 -0300

    estilizando com theme e condicionais.

--- src/CoolButton.jsx
+++ src/CoolButton.jsx
@@ -1,22 +1,28 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-// const useStyles = makeStyles({
-//     bottonStyle: {
-//         color: props => (props.cool ? "blue" : "red")
-//     }
-// });
-
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     bottonStyle: props => {
         return {
             color: props.cool ? "blue" : "red",
-            backgroundColor: props.cool ? "orange" : "yellow"
+            backgroundColor: props.cool ? "orange" : "yellow",
+            [theme.breakpoints.up("sm")]: {
+                // color: "cyan",
+                // backgroundColor: "black",
+                color: props.cool ? "cyan" : "whith",
+                backgroundColor: props.cool ? "black" : "blue",
+            },
         }
     }
-});
+    // bottonStyle: {
+    //     color: "red",
+    //     [theme.breakpoints.up("sm")]: {
+    //         color: "blue",
+    //     }
+    // }
+}));
 
 export default function Hook(props) {
     const classes = useStyles(props);
-    return <Button className={classes.bottonStyle}>the button</Button>;
+    return <Button fullWidth className={classes.bottonStyle}>the button</Button>;
 }
